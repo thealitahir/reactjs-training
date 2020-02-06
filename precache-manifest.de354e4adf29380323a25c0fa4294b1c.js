d4ea3c86ba77d2881a5f4ce41ea94a25bc537503
thealitahir <ali.tahir@Cdocs.local>
2020-02-06 18:30:10 +0500

    Updates

---
+++ precache-manifest.de354e4adf29380323a25c0fa4294b1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78f209c1da1d20a810485b97b22e06b",
+    "url": "/reactjs-training/index.html"
+  },
+  {
+    "revision": "e2482c8cef20df34ee9d",
+    "url": "/reactjs-training/static/css/main.aff59d47.chunk.css"
+  },
+  {
+    "revision": "d2bbe500a22e8b0539c3",
+    "url": "/reactjs-training/static/js/2.ead099bf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactjs-training/static/js/2.ead099bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2482c8cef20df34ee9d",
+    "url": "/reactjs-training/static/js/main.6f4e539e.chunk.js"
+  },
+  {
+    "revision": "0623113a2c3daf70f0ba",
+    "url": "/reactjs-training/static/js/runtime-main.6837479c.js"
+  },
+  {
+    "revision": "36d83c85482610808775021d1467efbf",
+    "url": "/reactjs-training/static/media/background.36d83c85.jpg"
+  },
+  {
+    "revision": "0034edd107418c435c91a8590d3fa8e2",
+    "url": "/reactjs-training/static/media/nbslogo.0034edd1.png"
+  },
+  {
+    "revision": "dfff02f8e42b226f4b27bc0fd1ac51b0",
+    "url": "/reactjs-training/static/media/pencil.dfff02f8.png"
+  }
+]);
