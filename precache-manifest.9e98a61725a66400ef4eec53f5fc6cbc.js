a0879d84b6ecccc700e32630d8826fb7119bb31d
thealitahir <ali.tahir@Cdocs.local>
2020-02-06 18:53:07 +0500

    Updates

---
+++ precache-manifest.9e98a61725a66400ef4eec53f5fc6cbc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f980070cd9e9be56edd01a66fdd13b4b",
+    "url": "/reactjs-training/index.html"
+  },
+  {
+    "revision": "a8a1640ae4e85a4c8506",
+    "url": "/reactjs-training/static/css/main.aff59d47.chunk.css"
+  },
+  {
+    "revision": "d2bbe500a22e8b0539c3",
+    "url": "/reactjs-training/static/js/2.ead099bf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/reactjs-training/static/js/2.ead099bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8a1640ae4e85a4c8506",
+    "url": "/reactjs-training/static/js/main.7d9f171c.chunk.js"
+  },
+  {
+    "revision": "0623113a2c3daf70f0ba",
+    "url": "/reactjs-training/static/js/runtime-main.6837479c.js"
+  },
+  {
+    "revision": "36d83c85482610808775021d1467efbf",
+    "url": "/reactjs-training/static/media/background.36d83c85.jpg"
+  },
+  {
+    "revision": "0034edd107418c435c91a8590d3fa8e2",
+    "url": "/reactjs-training/static/media/nbslogo.0034edd1.png"
+  },
+  {
+    "revision": "dfff02f8e42b226f4b27bc0fd1ac51b0",
+    "url": "/reactjs-training/static/media/pencil.dfff02f8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjs-training-github/precache-manifest.b8ac9501fe660148f114e9111f267e3e.js"
+  "/reactjs-training/precache-manifest.9e98a61725a66400ef4eec53f5fc6cbc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs-training-github/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs-training/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
